37e5a0176e75cb024a6a2ec981e8740161535a24
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2023-09-14 08:26:34 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"hello"<<endl;
+    for(int i =0 ; i>4;i++){
+    for(int j =0 ; j>4;j++){
+    for(int k =0 ; k>4;k++){
+        cout<<"hello"<<endl;
+    }    }
+        }
+    return 0;
+}
